4b3678a21468d0fd07842ff4537f2cc30c17ad35
Luciano Santos <luciano_z7@hotmail.com>
2021-09-22 08:25:26 -0300

    Adding inversions and sort functions

--- count_hobbies.go
+++ count_hobbies.go
@@ -1,6 +1,6 @@
 package dchobbies
 
-func MergeAndCountInversions(leftS, rightS []int) ([]int, int) {
+func mergeAndCountInversions(leftS, rightS []int) ([]int, int) {
 	var mergedS []int
 	mergedInv := 0
 	leftI, rightI := 0, 0
@@ -28,7 +28,7 @@ func MergeAndCountInversions(leftS, rightS []int) ([]int, int) {
 	return mergedS, mergedInv
 }
 
-func SplitSlice(s []int) (s1, s2 []int) {
+func splitSlice(s []int) (s1, s2 []int) {
 	if len(s)%2 == 0 {
 		s1 = s[:len(s)/2]
 		s2 = s[len(s)/2:]
@@ -37,4 +37,25 @@ func SplitSlice(s []int) (s1, s2 []int) {
 		s2 = s[(len(s)+1)/2:]
 	}
 	return
+}
+
+func sortAndCountInversions(s []int, inv int) ([]int, int) {
+	if len(s) == 1 {
+		return s, inv
+	} else {
+		s1, s2 := splitSlice(s)
+
+		leftS, leftInv := sortAndCountInversions(s1, inv)
+		rightS, rightInv := sortAndCountInversions(s2, inv)
+		mergedS, mergedInv := mergeAndCountInversions(leftS, rightS)
+
+		totalInv := leftInv + rightInv + mergedInv
+
+		return mergedS, totalInv
+	}
+}
+
+func CountSliceInversions(s []int) ([]int, int) {
+	totalSlice, totalInv := sortAndCountInversions(s, 0)
+	return totalSlice, totalInv
 }
